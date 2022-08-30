d7a289ce7738198ba5b78bcecb10525f493db403
AyagiKei <ayagikei@163.com>
2022-08-30 21:26:39 +0800

    feat: add upload archives shell

---
+++ uploadArchivesAll.sh
@@ -0,0 +1,7 @@
+./gradlew :core:uploadArchives
+./gradlew :bottomsheets:uploadArchives
+./gradlew :color:uploadArchives
+./gradlew :datetime:uploadArchives
+./gradlew :files:uploadArchives
+./gradlew :input:uploadArchives
+./gradlew :lifecycle:uploadArchives
